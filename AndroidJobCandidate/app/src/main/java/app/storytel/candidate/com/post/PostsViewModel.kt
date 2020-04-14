2fd43af58fbe84c33b7ed345d8a6ef8f90f4ac26
Etukeni E. Ndecha O <beettechnologies@gmail.com>
2020-04-15 00:21:02 +0200

    Implements UI for posts and PostItem

--- AndroidJobCandidate/app/src/main/java/app/storytel/candidate/com/post/PostsViewModel.kt
+++ AndroidJobCandidate/app/src/main/java/app/storytel/candidate/com/post/PostsViewModel.kt
@@ -1,5 +1,11 @@
 package app.storytel.candidate.com.post
 
 import app.storytel.candidate.com.common.base.BaseViewModel
+import app.storytel.candidate.com.post.domain.data.local.model.Post
 
-class PostsViewModel : BaseViewModel()
+class PostsViewModel : BaseViewModel() {
+
+    fun onPostItemClicked(post: Post) {
+
+    }
+}
