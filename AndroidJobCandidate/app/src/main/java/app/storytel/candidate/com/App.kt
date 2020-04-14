3800143fe04414bf110060da080c470cc61b4631
Etukeni E. Ndecha O <beettechnologies@gmail.com>
2020-04-14 16:53:39 +0200

    Setup Koin for DI and Timer for logging

--- AndroidJobCandidate/app/src/main/java/app/storytel/candidate/com/App.kt
+++ AndroidJobCandidate/app/src/main/java/app/storytel/candidate/com/App.kt
@@ -1,6 +1,30 @@
 package app.storytel.candidate.com
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import timber.log.Timber
 
 open class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        setupKoin()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+            // Connect Fabrics or Firebase here to handle
+        }
+    }
+
+    private fun setupKoin() {
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+            modules(listOf())
+        }
+    }
 }
